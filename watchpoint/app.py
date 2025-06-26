1b4460c10058b3259054308ebd1549630fdaff42
Leiver Moreno <leivermoreno@gmail.com>
2025-06-26 18:52:11 -0400

    enable session by providing secret key

--- watchpoint/app.py
+++ watchpoint/app.py
@@ -4,6 +4,9 @@ from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(SECRET_KEY="dev")
+    app.config.from_pyfile("config.py", silent=True)
+    app.config.from_prefixed_env()
 
     # create instance folder
     os.makedirs(app.instance_path, exist_ok=True)
