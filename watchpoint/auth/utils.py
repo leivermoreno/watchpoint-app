ba0888740ca31f1a8d3559642e47198fb123424b
Leiver Moreno <leivermoreno@gmail.com>
2025-07-13 15:25:08 -0400

    add auth middleware

--- watchpoint/auth/utils.py
+++ watchpoint/auth/utils.py
@@ -1,3 +1,19 @@
+from flask import g, redirect, url_for
+from functools import wraps
+
+
 def validate_credentials(nickname, password):
     if len(nickname) < 3 or len(password) < 7:
         return "Must provide, nickname >= 3 characters, and password >= 7 characters."
+
+
+def login_required(view):
+    @wraps(view)
+    def wrapper(*args, **kwargs):
+        if not g.user:
+            return redirect(url_for("auth.login"))
+
+        if view:
+            return view(*args, **kwargs)
+
+    return wrapper
