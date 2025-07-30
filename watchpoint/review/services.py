8e383b410fe06d43e2f37c132a3d744bd2f33808
Leiver Moreno <leivermoreno@gmail.com>
2025-07-29 21:24:35 -0400

    add vote counting on review view

--- watchpoint/review/services.py
+++ watchpoint/review/services.py
@@ -1,5 +1,5 @@
 from flask import g
-from sqlalchemy import select, desc, asc, func
+from sqlalchemy import select, desc, asc, func, case, cast, Integer
 from sqlalchemy.dialects.postgresql import insert
 from review.models import Review, Vote
 from db import db
@@ -9,7 +9,6 @@ REVIEW_SORT_OPTIONS = ["newest", "oldest"]
 
 
 def get_reviews(page, title_id, sort_by):
-    print(sort_by)
     offset = (page - 1) * REVIEW_PAGE_LIMIT
     stmt = select(Review).limit(REVIEW_PAGE_LIMIT).offset(offset)
     sort_func = desc if sort_by == "newest" else asc
@@ -18,9 +17,31 @@ def get_reviews(page, title_id, sort_by):
     if title_id:
         stmt = stmt.filter_by(title_id=title_id)
 
-    result = db.session.scalars(stmt).all()
+    reviews = db.session.scalars(stmt).all()
+    review_ids = [r.id for r in reviews]
+    if not review_ids:
+        return reviews
 
-    return result
+    vote_stmt = (
+        select(
+            Vote.review_id,
+            func.sum(case((Vote.upvote == True, 1), else_=0)).label("upvotes"),
+            func.sum(case((Vote.upvote == False, 1), else_=0)).label("downvotes"),
+            func.max(
+                case(
+                    (Vote.user_id == g.user.id, cast(Vote.upvote, Integer)), else_=None
+                )
+            ).label("user_upvote"),
+        )
+        .where(Vote.review_id.in_(review_ids))
+        .group_by(Vote.review_id)
+    )
+
+    vote_data = {row.review_id: row for row in db.session.execute(vote_stmt)}
+    for r in reviews:
+        r.vote_data = vote_data[r.id]
+
+    return reviews
 
 
 def get_review_count(title_id):
